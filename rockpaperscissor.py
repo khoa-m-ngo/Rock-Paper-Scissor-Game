38f9ab3234d4251493f74e432313d1f9c513c128
khoa-m-ngo <khoa.ngo.m@gmail.com>
2023-08-22 14:12:11 -0400

    initial gameplay improvement

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -3,12 +3,13 @@ import random
 # start = "n"
 # while(start != "y"):
 #     start = input("Are you ready? (y/n) \n")
+point = {"player": 0, "npc": 0}
+key = {"r":1, "p":2, "s":3, "1":"Rock", "2":"Paper", "3":"Scissor"}
 
 while(1):
-    key = {"r":1, "p":2, "s":3}
-    n = input("Your turn: ")
+    n = input("Your turn: \n")
     npc = random.randrange(1, 4)
-    
+
     if n not in key.keys():
         print("Enter a key: R, P, or S \n")
         continue
@@ -16,6 +17,27 @@ while(1):
     player = key[n]
     if player == npc:
         print("It's a draw")
+    elif player == 1 and npc == 2:
+        point["npc"] += 1
+    elif player == 1 and npc == 3:
+        point["player"] += 1
+    elif player == 2 and npc == 1:
+        point["player"] += 1
+    elif player == 2 and npc == 3:
+        point["npc"] += 1
+    elif player == 3 and npc == 1:
+        point["npc"] += 1
+    elif player == 3 and npc == 2:
+        point["player"] += 1
     
+    print("You chose " + key[str(player)] + ". Opponent chose " + key[str(npc)])
+    print(str(point) + "\n")
+
+    if point["player"] >= 3:
+        print("You won! \n")
+        break
+    elif point["npc"] >= 3:
+        print("You lost... \n")
+        break
 
 print("Game Finished.")
